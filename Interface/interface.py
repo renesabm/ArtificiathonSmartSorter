3d59c4a5f8994bd15cb4fd0c4d70e98d842fd0de
renesabm <renesabm@gmail.com>
2025-12-28 19:28:40 -0800

    Update interface.py

--- Interface/interface.py
+++ Interface/interface.py
@@ -7,7 +7,7 @@ import time
 
 @st.cache_resource
 def load_model():
-    model_path = "ArtificiathonSmartSorter/ArtificiathonMobileNetV2/ArtificiationMobileNetV2_battery1.keras"
+    model_path = "ArtificiationMobileNetV2_battery1.keras"
     return tf.keras.models.load_model(
         model_path,
         compile=False
@@ -80,6 +80,7 @@ if image:
     progress_bar.empty()
     status_text.empty()
     st.button("Classify another image", on_click=start_over)
+
 
 
 
